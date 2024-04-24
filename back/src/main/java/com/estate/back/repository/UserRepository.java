03752067cd7f046a32f26228b863cb98a6a40a4c
OREO206 <baek6654@naver.com>
2024-04-24 15:50:30 +0900

    2024.04.24

--- back/src/main/java/com/estate/back/repository/UserRepository.java
+++ back/src/main/java/com/estate/back/repository/UserRepository.java
@@ -9,5 +9,7 @@ import com.estate.back.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     
+    boolean existsByUserId(String userId);
+
     UserEntity findByUserId(String userId);
 }
